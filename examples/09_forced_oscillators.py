553c6ea59f5e3d064d62908e33350d648877c345
Noah Parsons <nomapa223@gmail.com>
2025-12-01 12:48:07 -0700

    Update 09_forced_oscillators.py

--- examples/09_forced_oscillators.py
+++ examples/09_forced_oscillators.py
@@ -11,7 +11,6 @@ Physics:
 - Natural frequency: ω₀ = √(k/m)
 - Resonance when ω_d = ω₀
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mechanics_dsl import PhysicsCompiler
@@ -43,22 +42,24 @@ compiler1 = PhysicsCompiler()
 omega_d1 = 0.5 * omega0
 F0 = 1.0
 
-dsl_below = f"""
-\\system{forced_oscillator_below}
+dsl_below = r"""
+\system{forced_oscillator_below}
 
-\\var{{x}}{{Position}}{{m}}
+\defvar{x}{Position}{m}
 
-\\parameter{{m}}{{1.0}}{{kg}}
-\\parameter{{k}}{{10.0}}{{N/m}}
-\\parameter{{F0}}{{1.0}}{{N}}
-\\parameter{{omega_d}}{{omega_d1:.3f}}{{rad/s}}
+\parameter{m}{1.0}{kg}
+\parameter{k}{10.0}{N/m}
+\parameter{F0}{1.0}{N}
+\parameter{omega_d}{%.3f}{rad/s}
 
-\\lagrangian{{\\frac{{1}}{{2}} * m * \\dot{{x}}^2 - \\frac{{1}}{{2}} * k * x^2}}
+\lagrangian{
+    \frac{1}{2} * m * \dot{x}^2 - \frac{1}{2} * k * x^2
+}
 
-\\force{{F0 * \\cos{{omega_d * t}}}}
+\force{F0 * \cos{omega_d * t}}
 
-\\initial{{x=0.0, x_dot=0.0}}
-"""
+\initial{x=0.0, x_dot=0.0}
+""" % omega_d1
 
 result1 = compiler1.compile_dsl(dsl_below)
 if not result1['success']:
@@ -72,29 +73,31 @@ solution1 = compiler1.simulate(t_span=(0, 20), num_points=2000)
 # ============================================================================
 
 print("\n" + "="*60)
-print("AT RESONANCE (ω_d = ω₀) - MAXIMUM AMPLITUDE!")
+print("AT RESONANCE (ω_d = ω₀)")
 print("="*60)
 
 compiler2 = PhysicsCompiler()
 
 omega_d2 = omega0
 
-dsl_resonance = f"""
-\\system{forced_oscillator_resonance}
+dsl_resonance = r"""
+\system{forced_oscillator_resonance}
 
-\\var{{x}}{{Position}}{{m}}
+\defvar{x}{Position}{m}
 
-\\parameter{{m}}{{1.0}}{{kg}}
-\\parameter{{k}}{{10.0}}{{N/m}}
-\\parameter{{F0}}{{1.0}}{{N}}
-\\parameter{{omega_d}}{{omega_d2:.3f}}{{rad/s}}
+\parameter{m}{1.0}{kg}
+\parameter{k}{10.0}{N/m}
+\parameter{F0}{1.0}{N}
+\parameter{omega_d}{%.3f}{rad/s}
 
-\\lagrangian{{\\frac{{1}}{{2}} * m * \\dot{{x}}^2 - \\frac{{1}}{{2}} * k * x^2}}
+\lagrangian{
+    \frac{1}{2} * m * \dot{x}^2 - \frac{1}{2} * k * x^2
+}
 
-\\force{{F0 * \\cos{{omega_d * t}}}}
+\force{F0 * \cos{omega_d * t}}
 
-\\initial{{x=0.0, x_dot=0.0}}
-"""
+\initial{x=0.0, x_dot=0.0}
+""" % omega_d2
 
 result2 = compiler2.compile_dsl(dsl_resonance)
 if not result2['success']:
@@ -115,22 +118,24 @@ compiler3 = PhysicsCompiler()
 
 omega_d3 = 2.0 * omega0
 
-dsl_above = f"""
-\\system{forced_oscillator_above}
+dsl_above = r"""
+\system{forced_oscillator_above}
 
-\\var{{x}}{{Position}}{{m}}
+\defvar{x}{Position}{m}
 
-\\parameter{{m}}{{1.0}}{{kg}}
-\\parameter{{k}}{{10.0}}{{N/m}}
-\\parameter{{F0}}{{1.0}}{{N}}
-\\parameter{{omega_d}}{{omega_d3:.3f}}{{rad/s}}
+\parameter{m}{1.0}{kg}
+\parameter{k}{10.0}{N/m}
+\parameter{F0}{1.0}{N}
+\parameter{omega_d}{%.3f}{rad/s}
 
-\\lagrangian{{\\frac{{1}}{{2}} * m * \\dot{{x}}^2 - \\frac{{1}}{{2}} * k * x^2}}
+\lagrangian{
+    \frac{1}{2} * m * \dot{x}^2 - \frac{1}{2} * k * x^2
+}
 
-\\force{{F0 * \\cos{{omega_d * t}}}}
+\force{F0 * \cos{omega_d * t}}
 
-\\initial{{x=0.0, x_dot=0.0}}
-"""
+\initial{x=0.0, x_dot=0.0}
+""" % omega_d3
 
 result3 = compiler3.compile_dsl(dsl_above)
 if not result3['success']:
@@ -184,7 +189,7 @@ axes[1, 0].plot(t2, x2, 'b-', linewidth=2, label='Response')
 axes[1, 0].plot(t2, F2, 'r--', linewidth=1, alpha=0.5, label='Driving force (scaled)')
 axes[1, 0].set_xlabel('Time (s)')
 axes[1, 0].set_ylabel('Position (m)')
-axes[1, 0].set_title(f'At Resonance (ω_d = {omega_d2:.2f} rad/s) - LARGE AMPLITUDE!')
+axes[1, 0].set_title(f'At Resonance (ω_d = {omega_d2:.2f} rad/s)')
 axes[1, 0].legend()
 axes[1, 0].grid(True, alpha=0.3)
 
@@ -257,9 +262,8 @@ print("1. RESONANCE: Maximum amplitude when ω_d = ω₀")
 print("2. Below resonance: Response in phase with driving force")
 print("3. Above resonance: Response out of phase (180° shift)")
 print("4. At resonance: Amplitude grows linearly with time (no damping)")
-print("5. Use \\force{} command for time-dependent forces")
+print("5. Use \force{} command for time-dependent forces")
 print("6. Time-dependent forces use 't' as the time variable")
 print("="*60)
 
 plt.show()
-
